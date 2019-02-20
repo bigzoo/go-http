14f489e1ebded11a4fa70a33587f2f91d8116aab
Chris Nyaga <nyagachris75@gmail.com>
2019-02-20 23:06:49 +0300

    wrote simple writer interface implementation

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
+
+type logWriter struct {}
 
 func main(){
 	resp, err := http.Get("http://google.com")
@@ -12,5 +15,13 @@ func main(){
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write (bs []byte) (int, error){
+	fmt.Println(string(bs))
+	return len(bs), nil
 }
